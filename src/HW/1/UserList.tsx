27485dc5d426e6dd7a4cd7e38a1fe52fc2369152
Anya Alesik <100134026+nayavii@users.noreply.github.com>
2026-02-19 12:50:03 +0100

    refactor: hm1

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,31 +1,36 @@
 type AddressType = {
-  street: string; 
-  city: string; 
+  street: string;
+  city: string;
 };
 
 type UserType = {
-  id: number
-  name: string
-  age: number
-  address: AddressType,
+  id: number;
+  name: string;
+  age: number;
+  address: AddressType;
 };
 
 type UserListPropsType = {
-  users: UserType[]; 
+  users: UserType[];
 };
 
-export const UserList = (props: UserListPropsType) => {
+export const UserList = ({ users }: UserListPropsType) => {
   return (
     <div id={'hw01-users'}>
       <h2>User List:</h2>
 
       <ul>
-       {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
-          <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>
-           <strong>{ЭЛЕМЕНТ МАССИВА.name}</strong> (Age: {ЭЛЕМЕНТ МАССИВА.age})<strong> Address:</strong>
-           {ЭЛЕМЕНТ МАССИВА.address.street}, {ЭЛЕМЕНТ МАССИВА.address.city}
-         </li>
-       ))}
+        {users.map(
+          (
+            user,
+          ) => (
+            <li key={user.id} id={`hw01-user-${user.id}`}>
+              <strong>{user.name}</strong> (Age: {user.age})
+              <strong> Address:</strong>
+              {user.address.street}, {user.address.city}
+            </li>
+          ),
+        )}
       </ul>
     </div>
   );
